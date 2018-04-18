13f0e051d94510db41f3479beef56a51ac082097
Jonata William <jonatawilliam@gmail.com>
2018-04-18 19:52:40 -0300

    Adicionado diretorio das imagens de Arquitetura do Software que estavam no projeto antigo, antes do Fork

--- 1-tier-desk-app/src/main/java/com/mycompany/mavenproject1/dao/Conexao.java
+++ 1-tier-desk-app/src/main/java/com/mycompany/mavenproject1/dao/Conexao.java
@@ -9,7 +9,7 @@ public class Conexao {
     private static Connection connection;
     private static String dbURL = "jdbc:mysql://localhost:3306/saapp";
     private static String username = "root";
-    private static String password = "gabriel";
+    private static String password = "";
     public static Connection getConnection() throws SQLException {
         if (connection == null) {
             connection = DriverManager.getConnection(dbURL, username, password);
